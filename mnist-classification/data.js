20855b925361a5b43103f54af852fbf9a93e2411
Rising <risingodegua@gmail.com>
2020-05-11 23:00:17 +0100

    Finish model loading and UI rendering

--- mnist-classification/data.js
+++ mnist-classification/data.js
@@ -20,7 +20,6 @@ const LABEL = []
 
 async function loadData() {
     const csvDataset = tf.data.csv(TRAIN_DATA, {
-        hasHeader: true,
         columnConfigs: {
             label: { isLabel: true }
         }
@@ -31,7 +30,6 @@ async function loadData() {
 
 
 async function process(row) {
-    // TENSOR_DATA.push(tf.tensor((Object.values(row['xs'])), [IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL]).div(255))
     TENSOR_DATA.push(Object.values(row['xs']))
     LABEL.push(Object.values(row['ys'])[0])
 }
@@ -55,3 +53,80 @@ exports.MnistClass = class MnistDataset {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const tf = require('@tensorflow/tfjs-node');
+// const model = require('./model')
+
+
+// const BASE_PATH = "file:///home/dsn/personal/Tfjs/TensorFlowjs_Projects/mnist-classification/dataset/";
+// const TRAIN_DATA = `${BASE_PATH}train.csv`;
+// const IMAGE_WIDTH = 28
+// const IMAGE_HEIGHT = 28
+// const IMAGE_CHANNEL = 1
+// const IMAGE_SIZE = 784;
+// const NUM_CLASSES = 10;
+// const DATA_SIZE = 42000
+// const NUM_TRAIN_DATA = 38000
+// const NUM_TEST_DATA = DATA_SIZE - NUM_TRAIN_DATA
+
+// const TENSOR_DATA = []
+// const LABEL = []
+
+
+// async function loadData() {
+//     const csvDataset = tf.data.csv(TRAIN_DATA, {
+//         columnConfigs: {
+//             label: { isLabel: true }
+//         }
+//     });
+//     await csvDataset.forEachAsync(row => process(row));
+
+// }
+
+
+// async function process(row) {
+//     TENSOR_DATA.push(Object.values(row['xs']))
+//     LABEL.push(Object.values(row['ys'])[0])
+// }
+
+
+// exports.MnistClass = class MnistDataset {
+//     constructor() {
+//         this.Xtrain = null
+//         this.Xval = null
+//         this.ytrain = null
+//         this.ytest = null
+//     }
+
+//     async startDataLoading() {
+//         // dat = new MnistDataset()
+//         await loadData()
+//         this.Xtrain = tf.tensor(TENSOR_DATA.slice(0, NUM_TRAIN_DATA)).reshape([NUM_TRAIN_DATA, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL]).div(255)
+//         this.Xtest = tf.tensor(TENSOR_DATA.slice(NUM_TRAIN_DATA)).reshape([NUM_TEST_DATA, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL]).div(255)
+//         this.ytrain = tf.oneHot(tf.tensor1d(LABEL.slice(0, NUM_TRAIN_DATA), 'int32'), NUM_CLASSES);
+//         this.ytest = tf.oneHot(tf.tensor1d(LABEL.slice(NUM_TRAIN_DATA), 'int32'), NUM_CLASSES);
+//     }
+
+// }
---
+++ mnist-classification/public/js/index.js
@@ -0,0 +1,169 @@
+/***
+ * https://github.com/Gogul09/digit-recognizer-live/blob/master/js/app.js
+ */
+
+const canvasWidth = 400;
+const canvasHeight = 400;
+const canvasStrokeStyle = "white";
+const canvasLineJoin = "round";
+const canvasLineWidth = 5;
+const canvasBackgroundColor = "black";
+const canvasId = "canvas";
+
+var clickX = new Array();
+var clickY = new Array();
+var clickD = new Array();
+var drawing;
+
+// document.getElementById('chart_box').innerHTML = "";
+// document.getElementById('chart_box').style.display = "none";
+
+//---------------
+// Create canvas
+//---------------
+const canvasBox = document.getElementById('canvasDiv');
+const canvas = document.createElement("canvas");
+
+canvas.setAttribute("width", canvasWidth);
+canvas.setAttribute("height", canvasHeight);
+canvas.setAttribute("id", canvasId);
+canvas.style.backgroundColor = canvasBackgroundColor;
+canvasBox.appendChild(canvas);
+
+if (typeof G_vmlCanvasManager != 'undefined') {
+    canvas = G_vmlCanvasManager.initElement(canvas);
+}
+
+ctx = canvas.getContext("2d");
+
+//---------------------
+// MOUSE DOWN function
+//---------------------
+$("#canvas").mousedown(function (e) {
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop;
+
+    drawing = true;
+    addUserGesture(mouseX, mouseY);
+    drawOnCanvas();
+});
+
+//---------------------
+// MOUSE MOVE function
+//---------------------
+$("#canvas").mousemove(function (e) {
+    if (drawing) {
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+        addUserGesture(mouseX, mouseY, true);
+        drawOnCanvas();
+    }
+});
+
+
+//-------------------
+// MOUSE UP function
+//-------------------
+$("#canvas").mouseup(function (e) {
+    drawing = false;
+});
+
+//----------------------
+// MOUSE LEAVE function
+//----------------------
+$("#canvas").mouseleave(function (e) {
+    drawing = false;
+});
+
+
+//--------------------
+// ADD CLICK function
+//--------------------
+function addUserGesture(x, y, dragging) {
+    clickX.push(x);
+    clickY.push(y);
+    clickD.push(dragging);
+}
+
+//-------------------
+// RE DRAW function
+//-------------------
+function drawOnCanvas() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    ctx.strokeStyle = canvasStrokeStyle;
+    ctx.lineJoin = canvasLineJoin;
+    ctx.lineWidth = canvasLineWidth;
+
+    for (var i = 0; i < clickX.length; i++) {
+        ctx.beginPath();
+        if (clickD[i] && i) {
+            ctx.moveTo(clickX[i - 1], clickY[i - 1]);
+        } else {
+            ctx.moveTo(clickX[i] - 1, clickY[i]);
+        }
+        ctx.lineTo(clickX[i], clickY[i]);
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+//------------------------
+// CLEAR CANVAS function
+//------------------------
+function clearCanvas(id) {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    clickX = new Array();
+    clickY = new Array();
+    clickD = new Array();
+}
+
+
+
+tstatus = document.getElementById('status')
+predval = document.getElementById('predval')
+
+
+//Load the CNN Model
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadModel();
+}, false);
+
+
+async function loadModel() {
+    tstatus.innerText = 'Loading Model...'
+    model = await tf.loadLayersModel("http://localhost:3000/assets/model/model.json");
+    tstatus.innerText = 'Model Loaded Successfully. Start Drawing!'
+
+    // model.summary()
+}
+
+
+/**
+ * Preprocess the Image drawn on canvas and reshape it to [1,28,28,1]
+ */
+function getImageFromCanvas(image) {
+
+    let tensor = tf.browser.fromPixels(image)
+        .resizeNearestNeighbor([28, 28])
+        .mean(2)
+        .expandDims(2)
+        .expandDims()
+        .toFloat()
+        .div(255.0)
+    return tensor
+
+}
+
+
+async function predict() {
+
+    // preprocess canvas
+    let tensor = getImageFromCanvas(canvas);
+
+    // make predictions on the preprocessed image tensor
+    let pred = await model.predict(tensor).argMax([-1])
+    // tf.print(pred)
+    console.log(pred.arraySync()[0])
+    predval.innerText = pred.arraySync()[0]
+}
