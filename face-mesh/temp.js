9146835f079c16b120a62ebc819e7510fcf2b7d8
Rising <risingodegua@gmail.com>
2020-07-05 17:06:31 +0100

    Removed folders

--- face-mesh/temp.js
+++ face-mesh/temp.js
@@ -4,6 +4,9 @@ let webcamElement = document.getElementById("webcam")
 let capturing = false
 
 async function capture() {
+    // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain an
+    // array of detected faces from the MediaPipe graph.
+    // webcam = await tf.data.webcam(webcamElement);
     capturing = true
 
     while (capturing) {
@@ -11,7 +14,36 @@ async function capture() {
         const predictions = await model.estimateFaces(img);
 
         if (predictions.length > 0) {
-           
+            /*
+            `predictions` is an array of objects describing each detected face, for example:
+        
+            [
+              {
+                faceInViewConfidence: 1, // The probability of a face being present.
+                boundingBox: { // The bounding box surrounding the face.
+                  topLeft: [232.28, 145.26],
+                  bottomRight: [449.75, 308.36],
+                },
+                mesh: [ // The 3D coordinates of each facial landmark.
+                  [92.07, 119.49, -17.54],
+                  [91.97, 102.52, -30.54],
+                  ...
+                ],
+                scaledMesh: [ // The 3D coordinates of each facial landmark, normalized.
+                  [322.32, 297.58, -17.54],
+                  [322.18, 263.95, -30.54]
+                ],
+                annotations: { // Semantic groupings of the `scaledMesh` coordinates.
+                  silhouette: [
+                    [326.19, 124.72, -3.82],
+                    [351.06, 126.30, -3.00],
+                    ...
+                  ],
+                  ...
+                }
+              }
+            ]
+            */
             let a = []; b = []; c = []
             for (let i = 0; i < predictions.length; i++) {
                 const keypoints = predictions[i].mesh;
@@ -21,6 +53,7 @@ async function capture() {
                     a.push(y)
                     b.push(x)
                     c.push(z)
+                    // console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
                 }
             }
 
@@ -35,7 +68,7 @@ async function capture() {
                     z: c,
                 }
             ];
-            Plotly.newPlot('plot', data)
+            Plotly.newPlot('myDiv', data);
 
         }
     }
@@ -58,6 +91,7 @@ async function main() {
         capturing = false
     })
 }
+
 
 
 main();
